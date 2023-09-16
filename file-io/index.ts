0fffcbb1e01ead7e378dbc54325111aeb75f2948
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-16 10:46:37 +0200

    file-io: implement writeToFile(), readFileAsString() and readFileAsStream() functions

--- file-io/index.ts
+++ file-io/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+console.log('File IO')
+
+const writeToFile = (filename: string) => {
+  const content = 'first line\nsecond line'
+  Bun.write(filename, content)
+}
+
+const readFileAsString = async (filename: string) => {
+  const file = Bun.file(filename)
+  return await file.text()
+}
+
+const readFileAsStream = async (filename: string) => {
+  const file = Bun.file(filename)
+  const reader = file.stream().getReader()
+  const data = (await reader.read()).value
+  const content = new TextDecoder().decode(data)
+  console.log(content)
+}
+
+writeToFile('file.txt')
+
+// readFileAsString('file.txt')
+readFileAsStream('file.txt')
